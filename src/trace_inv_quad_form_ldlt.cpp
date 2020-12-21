207e6b5d29b055adcf650d297740092e96c690ec
Ben <bbbales2@gmail.com>
2020-12-21 15:19:50 -0500

    Added vector test to trace_inv_quad_form

--- src/trace_inv_quad_form_ldlt.cpp
+++ src/trace_inv_quad_form_ldlt.cpp
@@ -25,9 +25,31 @@ static void trace_inv_quad_form_ldlt(benchmark::State& state) {
   callback_bench_impl(init, run, state);
 }
 
+static void trace_inv_quad_form_ldlt_vector(benchmark::State& state) {
+  using stan::math::var;
+  using stan::math::promote_scalar;
+  
+  auto init = [](benchmark::State& state) {
+    Eigen::MatrixXd x_val = Eigen::MatrixXd::Random(state.range(0), state.range(0));
+    Eigen::VectorXd y_val = Eigen::VectorXd::Random(state.range(0));
+
+    stan::math::LDLT_factor<var, Eigen::Dynamic, Eigen::Dynamic>
+    x_ldlt(CAST_VAR(x_val));
+    
+    return std::make_tuple(x_ldlt, CAST_VAR(y_val));
+  };
+
+  auto run = [](const auto&... args) {
+    return trace_inv_quad_form_ldlt(args...);
+  };
+
+  callback_bench_impl(init, run, state);
+}
+
 // The start and ending sizes for the benchmark
 int start_val = 2;
 int end_val = 1024;
 BENCHMARK(toss_me);
 BENCHMARK(trace_inv_quad_form_ldlt)->RangeMultiplier(2)->Range(start_val, end_val)->UseManualTime();
+BENCHMARK(trace_inv_quad_form_ldlt_vector)->RangeMultiplier(2)->Range(start_val, end_val)->UseManualTime();
 BENCHMARK_MAIN();
